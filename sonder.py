6fa36b32925564e3bee3e2a3172b1da9145cecf7
mattew20201 <mattew20201@protonmail.com>
2024-06-12 22:22:10 -0700

    Add files via upload

--- sonder.py
+++ sonder.py
@@ -25,11 +25,17 @@ with open(file_useano, 'r') as file:
 if file_content1 != file_content2:
   print("YES")
   with open(file_useano, 'w') as file:
+     print("YES1")
      message=toreplace.replace(file_content2, '')
+     print("YES2")
      file.write(toreplace)
+     print("YES3")
      file.close()
+     print("YES4")
      url=f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
+     print("YES5")
      requests.get(url)
+     print("YES6")
      
   
   
